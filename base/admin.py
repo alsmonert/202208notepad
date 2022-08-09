746736917f205b05aa478c44b9d0ad93ec9a2514
alsxonert <dongxin1@msu.edu>
2022-08-09 13:11:32 -0400

    update

---
+++ base/admin.py
@@ -0,0 +1,6 @@
+from tkinter.tix import Form
+from django.contrib import admin
+from .models import Task
+# Register your models here.
+
+admin.site.register(Task)
---
+++ base/urls.py
@@ -0,0 +1,17 @@
+import imp
+from re import L
+from django.urls import path
+from .views import TaskList, TaskDetail, TaskCreat, TaskUpdate, DeleteView, CustomLoginView, ResigterPage
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', ResigterPage.as_view(), name='register'),
+
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create', TaskCreat.as_view(), name='task-create'),
+    path('task-edit/<int:pk>/', TaskUpdate.as_view(), name='task-edit'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+]
